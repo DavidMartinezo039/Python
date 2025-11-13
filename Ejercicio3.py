b47de7f6fc9e7b1e06e8df59fc5afc203e6c03b7
David Martínez Aliaga <158084789+DavidMartinezo039@users.noreply.github.com>
2025-11-13 19:17:18 +0100

    Tipos de variables, y constantes en fichero externo

---
+++ Ejercicio3.py
@@ -0,0 +1,13 @@
+variable = "David"
+variable2 = 15
+variable3 = False
+print(variable, variable2, variable3)
+
+import Constantes
+print(Constantes.PI)
+
+variable4 = variable5 = variable6 = "David"
+print(variable4, variable5, variable6)
+
+variable4, variable5, variable6 = "David", "Pablo", "María"
+print(variable4, variable5, variable6)
