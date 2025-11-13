0e485c612b1f4aff942379807cc0e0e23fd54167
David Martínez Aliaga <158084789+DavidMartinezo039@users.noreply.github.com>
2025-11-13 19:27:24 +0100

    Solicitar diferentes tipos de datos

---
+++ Ejercicio4.py
@@ -0,0 +1,14 @@
+cadena = input("Introduce una palabra:")
+print(cadena)
+print(type(cadena))
+
+# Cambiar el tipo de dato para que no sea tipo texto y sea tipo numerico
+numero = int(input("Introduce una número:"))
+print(numero)
+print(type(numero))
+
+# tipo decimal
+decimal = float(input("Introduce una número:"))
+print(decimal)
+print(type(decimal))
+
